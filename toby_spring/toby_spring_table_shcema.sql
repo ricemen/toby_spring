be07c7a31b5032e35185f5053d37153a34be391c
ricemen <ricemen@gmail.com>
2017-03-09 17:51:42 +0900

    383 page2

--- toby_spring/toby_spring_table_shcema.sql
+++ toby_spring/toby_spring_table_shcema.sql
@@ -1,8 +1,10 @@
 create table users(
 	id varchar(10) primary key, 
 	name varchar(20) not null, 
+	email varchar(100) not null, 
 	password varchar(10) not null, 
 	level tinyint(3) not null, 
 	login int(3) not null, 
 	recommend int(3) not null
 )
+
