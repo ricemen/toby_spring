4f700e7dcc978e45b3eef29e4079d4f12adf7b05
wonseok <ricemen@outlook.kr>
2017-03-13 23:40:43 +0900

    500 page

---
+++ toby_spring/src/springbook/learningtest/spring/pointcut/PointcutTest.java
@@ -0,0 +1,61 @@
+package springbook.learningtest.spring.pointcut;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
+
+public class PointcutTest {
+
+	@Test
+	public void methodSignaturePointcut() throws NoSuchMethodException, SecurityException {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression("execution(public int " + 
+				"springbook.learningtest.spring.pointcut.Target.minus(int, int) " + 
+				"throws java.lang.RuntimeException)");
+		
+		// target minus
+		assertThat(pointcut.getClassFilter().matches(Target.class) && 
+				pointcut.getMethodMatcher().matches(Target.class.getMethod("minus", int.class, int.class), null), is(true));
+		
+		// target plus
+		assertThat(pointcut.getClassFilter().matches(Target.class) && 
+				pointcut.getMethodMatcher().matches(Target.class.getMethod("plus", int.class, int.class), null), is(false));
+		
+		// Bean.method() 
+		assertThat(pointcut.getClassFilter().matches(Bean.class) && 
+				pointcut.getMethodMatcher().matches(Target.class.getMethod("method"), null), is(false));
+	}
+	
+	@Test
+	public void pointcut() throws Exception {
+		targetClassPointcutMatches("execution(* *(..))", true, true, true, true, true, true);
+		targetClassPointcutMatches("execution(* hello(..))", true, true, false, false, false, false);
+		targetClassPointcutMatches("execution(* hello(String))", false, true, false, false, false, false);
+		targetClassPointcutMatches("execution(* meth*(..))", false, false, false, false, true, true);
+		targetClassPointcutMatches("execution(* *(int,int))", false, false, true, true, false, false);
+		targetClassPointcutMatches("execution(* *())", true, false, false, false, true, true);
+		targetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut.Target.*(..))", true, true, true, true, true, false);
+		targetClassPointcutMatches("execution(* springbook.learningtest.spring.pointcut.*.*(..))", true, true, true, true, true, true);
+	}
+	
+	public void targetClassPointcutMatches(String expression, boolean... expected) throws Exception {
+		pointcutMatches(expression, expected[0], Target.class, "hello");
+		pointcutMatches(expression, expected[1], Target.class, "hello", String.class);
+		pointcutMatches(expression, expected[2], Target.class, "plus", int.class, int.class);
+		pointcutMatches(expression, expected[3], Target.class, "minus", int.class, int.class);
+		pointcutMatches(expression, expected[4], Target.class, "method");
+		pointcutMatches(expression, expected[5], Bean.class, "method");
+	}
+	
+	public void pointcutMatches(String expression, Boolean expected, Class<?> clazz, String methodName, Class<?>... args) throws Exception {
+		AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+		pointcut.setExpression(expression);
+		
+		assertThat(pointcut.getClassFilter().matches(clazz) && 
+				pointcut.getMethodMatcher().matches(clazz.getMethod(methodName, args), null), is(expected));
+
+	}
+	
+}
---
+++ toby_spring/src/springbook/learningtest/spring/pointcut/Target.java
@@ -0,0 +1,28 @@
+package springbook.learningtest.spring.pointcut;
+
+public class Target implements TargetInterface {
+
+	@Override
+	public void hello() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void hello(String a) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int minus(int a, int b) throws RuntimeException {
+		return 0;
+	}
+
+	@Override
+	public int plus(int a, int b) {
+		return 0;
+	}
+	public void method() {}
+}
+
